0f7f1ee90103fe6bb758f2dee09edba07a54ca51
bajrushivlora <bajrushivlora@gmail.com>
2019-04-12 08:12:33 -0400

    done

--- scripts.js
+++ scripts.js
@@ -18,19 +18,19 @@ function draw() {
     line(x1, y1, x1, y1);
 
     if (keyCode == UP_ARROW) {
-        y1 = y1-6;  
+        y1 = y1-5;  
         stroke('red');
     } 
     else if (keyCode == DOWN_ARROW) {
-        y1 = y1+6;
+        y1 = y1+5;
         stroke('#42f44b');
     }
     else if (keyCode == LEFT_ARROW) {
-        x1 = x1-6;
+        x1 = x1-5;
         stroke('yellow');
     }
     else if (keyCode == RIGHT_ARROW) {
-        x1 = x1+6;
+        x1 = x1+5;
         stroke('blue');
     }
 }  
